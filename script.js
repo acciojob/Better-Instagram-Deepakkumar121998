613336fb015417fb2df17c205db7053793c1c060
Deepakkumar121998 <130857964+Deepakkumar121998@users.noreply.github.com>
2023-08-04 01:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+// Drag and drop functionality
+let dragged;
+
+document.addEventListener("dragstart", function (event) {
+  dragged = event.target;
+});
+
+document.addEventListener("dragover", function (event) {
+  event.preventDefault();
+});
+
+document.addEventListener("drop", function (event) {
+  event.preventDefault();
+  if (event.target.classList.contains("grid-item")) {
+    // Swap background images
+    const temp = dragged.style.backgroundImage;
+    dragged.style.backgroundImage = event.target.style.backgroundImage;
+    event.target.style.backgroundImage = temp;
+  }
+});
